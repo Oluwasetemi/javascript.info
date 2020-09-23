33014545fabe28e20e8e1ccd17c97088eade10d5
Ojo Oluwasetemi <setemiojo@gmail.com>
2020-09-23 13:12:53 +0100

    try to commit

--- code-quality/static-analysis/src/example.js
+++ code-quality/static-analysis/src/example.js
@@ -4,18 +4,7 @@ const val = 5;
 
 const obj = { name: 'ade', age: 99 };
 
-console.log(obj
-
-
-
-
-
-
-
-
-
-
-);
+console.log(obj);
 
 // eslint-disable-next-line no-unused-vars
 const sum = a => a;
