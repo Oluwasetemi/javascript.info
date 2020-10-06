9ba1a759be8197f71a41044a17452fef3d35d6a4
Ojo Oluwasetemi <setemiojo@gmail.com>
2020-10-06 13:11:15 +0100

    📦 NEW: fetch class

---
+++ fetch/index.js
@@ -0,0 +1,12 @@
+// fetch
+// formData
+// Fetch: Download progress
+// Fetch:Abort
+// Fetch: Cross-Origin Requests
+// Fetch API
+// URL objects
+// XMLHttpRequest
+// Resumable file upload
+// Long polling
+// WebSocket
+// Server Sent Events
