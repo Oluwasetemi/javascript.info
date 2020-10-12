2424168e65214255703c0054f610243366cb11bc
Ojo Oluwasetemi <setemiojo@gmail.com>
2020-10-12 13:41:52 +0100

    📦 NEW: add sum to client side

--- code-quality/automated_testing/lessons/client-side/task.js
+++ code-quality/automated_testing/lessons/client-side/task.js
@@ -1,4 +1,8 @@
 describe("Raises x to power n", function () {
+  it("5 in the power of 0 equals 0", function () {
+    assert.equal(pow(5, 0), 1);
+    expect(pow(5, 0)).to.equal(1)
+  });
   it("5 in the power of 1 equals 5", function () {
     assert.equal(pow(5, 1), 5);
   });
@@ -9,5 +13,23 @@ describe("Raises x to power n", function () {
 
   it("5 in the power of 3 equals 125", function () {
     assert.equal(pow(5, 3), 125);
+  });
+  it("5 in the power of -3 equals 1/125", function () {
+    assert.equal(pow(5, -3), 1/125);
+  });
+});
+describe("Sum function", function () {
+  it('return 7 for 3, 4 ', () => {
+    const result = sum(3, 4);
+    const expected = 7;
+    assert.equal(result, expected);
+  });
+  it('it accepts 2 number as argument ', () => {
+    expect(() => sum(3)).throw()
+  });
+
+  it('the 2 argument should be number', () => {
+    // expect(() => sum('a', 'b')).throw()
+    expect(() => sum('a', 7)).throw()
   });
 });
