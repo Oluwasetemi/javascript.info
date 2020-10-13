528b3e4babc0cf69b4a839d1442059184c376ad3
Ojo Oluwasetemi <setemiojo@gmail.com>
2020-10-13 14:14:03 +0100

    test

---
+++ code-quality/static-analysis2/src/example.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+console.log('hello');
+// console.log(++y);
+
+/* eslint-disable */
+console.log(typeof 5);
+
+/* eslint-enable */
+
+for (let i = 0; i < 10; i++) {
+	console.log('helllo');
+}
+
+let obj = {
+	name: 'ade',
+	age: 99,
+};
+
+// console.log(obj);
